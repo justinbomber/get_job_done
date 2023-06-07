da803b29d8f38e55de84f3011577ce64e5ac3c18
justinbomber <w0913706494@gmail.com>
2023-06-07 21:26:55 +0800

    add the first test

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 73, 255, 2)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -59,50 +34,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // 此次調用setState通知Flutter框架，在這個State中發生了變化，這導致它重新運行下面的build方法，
+      // 以便顯示可以反映更新的值。如果我們在不調用setState()的情況下更改_counter，
+      // 那麼build方法將不會再次被調用，因此似乎什麼都沒有發生。
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // 此方法每次調用setState時都會重新運行，例如上面的_incrementCounter方法。
+
+    // Flutter框架已經進行了優化，使得重新運行build方法變得快速，
+    // 因此您可以只重新構建需要更新的內容，而不必個別更改小部件的實例。
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        // 試試這個：在這裡更改顏色為特定的顏色（例如Colors.amber），
+        // 然後觸發熱重新加載，看看AppBar的顏色是否改變，而其他顏色保持不變。
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // 在這裡，我們從App.build方法創建的MyHomePage對象中獲取值，
+        // 並將其用於設置appbar的標題。
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        // Center是一個布局小部件。它接收一個子小部件並將其定位在父元素的中間。
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          // Column也是一個布局小部件。它接收一個子小部件列表並垂直排列它們。
+          // 默認情況下，它在水平方向上尺寸自適應其子小部件，
+          // 並嘗試與父元素一樣高。
+
+          // Column具有各種屬性來控制自身的尺寸和子小部件的位置。
+          // 在這裡，我們使用mainAxisAlignment來將子小部件在垂直方向上居中對齊；
+          // 這裡的主軸是垂直軸，因為Column是垂直的（橫軸將是水平的）。
+
+          // 試試這個：啟用"debug painting"（在IDE中選擇"Toggle Debug Paint"操作，
+          // 或在控制台中按"p"），查看每個小部件的框架線條。
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -119,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // 此逗號使build方法的自動格式化更好看。
     );
   }
 }
