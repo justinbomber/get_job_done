3c3a139837c7dac8612892089e96de4d4630f52f
justinbomber <w0913706494@gmail.com>
2023-06-10 16:49:17 +0800

    next bubble changes

--- lib/learn.dart
+++ lib/learn.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { //app的主要界面框架
   const MyApp({super.key});
 
   @override
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: MyHomePage(),
       ),
@@ -25,27 +25,70 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {
+class MyAppState extends ChangeNotifier { //app的狀態，運行所需要用到的數據 隨時變換
   var current = WordPair.random();
+  void getnext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatelessWidget { //app的主要部件
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+    final theme = Theme.of(context); // ?取?前主?
+    var the_style = theme.textTheme.displayMedium!.copyWith(
+      fontSize: 30,
+      color: const Color.fromARGB(100, 9, 32, 115),
+    );
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-                    ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random idea:', style: the_style),
+            Bigcard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                appState.getnext();
+              },
+              child: const Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class Bigcard extends StatelessWidget {
+  const Bigcard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final cardColor = Color.fromARGB(106, 108, 118, 145);
+    final cardLuminance = cardColor.computeLuminance();
+    final fontColor = cardLuminance > 0.5 ? Colors.black : Colors.white;
+    final style = theme.textTheme.displayMedium!.copyWith(
+      fontSize: 30,
+      color: fontColor,
+    );
+
+    return Card(
+      color: cardColor,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
